0d10f424b3567970a810a63a6161dd0445414e61
rcliu <liurchen@hotmail.com>
2020-03-21 11:36:01 +0800

    rotate

--- src/com/java/leetcode/Tag_Array.java
+++ src/com/java/leetcode/Tag_Array.java
@@ -2,6 +2,21 @@ package com.java.leetcode;
 
 public class Tag_Array {
     public void rotate(int[][] matrix) {
-        
+        // 先转置
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        // 再镜像对称
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = tmp;
+            }
+        }
     }
 }
