ba39e9a8711dfda7ac6cd1f910821c1899c2b7c6
rcliu <liurchen@hotmail.com>
2020-04-15 14:04:10 +0800

    up

--- src/com/java/leetcode/Tag_BFS.java
+++ src/com/java/leetcode/Tag_BFS.java
@@ -81,6 +81,121 @@ public class Tag_BFS {
     public int maxDistance(int[][] grid) {
         return 0;
     }
+
+    // id 429
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> tmp = new ArrayList<>();
+            while (size > 0) {
+                Node nd = q.poll();
+                assert nd != null;
+                tmp.add(nd.val);
+                size--;
+                for (Node node : nd.children) {
+                    q.offer(node);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    // id 513
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        List<List<Integer>> res = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            while (size > 0) {
+                TreeNode tmp = q.poll();
+                size--;
+                assert tmp != null;
+                level.add(tmp.val);
+                if (tmp.left != null) {
+                    q.offer(tmp.left);
+                }
+                if (tmp.right != null) {
+                    q.offer(tmp.right);
+                }
+            }
+            res.add(level);
+        }
+        return res.get(res.size() - 1).get(0);
+    }
+
+    // id 199
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        List<List<Integer>> res = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            while (size > 0) {
+                TreeNode tmp = q.poll();
+                size--;
+                assert tmp != null;
+                level.add(tmp.val);
+                if (tmp.left != null) {
+                    q.offer(tmp.left);
+                }
+                if (tmp.right != null) {
+                    q.offer(tmp.right);
+                }
+            }
+            res.add(level);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (List<Integer> le : res) {
+            result.add(le.get(le.size() - 1));
+        }
+        return result;
+    }
+
+    // id 515
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        List<List<Integer>> res = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            while (size > 0) {
+                TreeNode tmp = q.poll();
+                size--;
+                assert tmp != null;
+                level.add(tmp.val);
+                if (tmp.left != null) {
+                    q.offer(tmp.left);
+                }
+                if (tmp.right != null) {
+                    q.offer(tmp.right);
+                }
+            }
+            res.add(level);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (List<Integer> le : res) {
+            int max = le.get(0);
+            for (int val : le) {
+                if (max < val) {
+                    max = val;
+                }
+            }
+            result.add(max);
+        }
+        return result;
+    }
 }
 
 // Employee info
